770187fe8a34ddfa93467a56494eaabedb78ac6b
Mika <imikado@gmail.com>
2021-02-17 21:04:58 +0100

    suppression get_magic_quotes (deprecie depuis

--- lib/framework/class_root.php
+++ lib/framework/class_root.php
@@ -206,14 +206,6 @@ class _root{
 				self::getAuth()->enable();
 			}
 
-			//desactivation des magic quotes
-			if (get_magic_quotes_gpc()) {
-				$_POST = array_map('stripslashes_deep', $_POST);
-				$_GET = array_map('stripslashes_deep', $_GET);
-				$_COOKIE = array_map('stripslashes_deep', $_COOKIE);
-				$this->getRequest()->magic_quote();
-			}
-
 			if( (int)self::getConfigVar('urlrewriting.enabled') == 1 ){
 				self::getUrlRewriting()->parseUrl($_SERVER['REQUEST_URI']);
 			}
